a6e56d311d3fe0fa0e0b07aab6b1c976f09cd2cb
AlexandreMoreira-FIAP <RM363194@fiap.com.br>
2025-04-29 20:53:36 -0300

    Feat: Aplicação inicial do Restaurante

---
+++ restmanager/src/main/java/br/com/posfiap/restmanager/RestManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.posfiap.restmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestManagerApplication.class, args);
+	}
+
+}
---
+++ restmanager/src/test/java/br/com/posfiap/restmanager/RestManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.posfiap.restmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
