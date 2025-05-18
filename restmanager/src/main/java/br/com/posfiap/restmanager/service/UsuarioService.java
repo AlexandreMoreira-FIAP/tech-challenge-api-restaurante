cec1806a8f1e3bdf546a0cf1fbb19c29fa5bf98c
JGontijo <jhonathan_gontijo@hotmail.com>
2025-05-18 16:57:26 -0300

    Adicionando criptografia da senha

--- restmanager/src/main/java/br/com/posfiap/restmanager/service/UsuarioService.java
+++ restmanager/src/main/java/br/com/posfiap/restmanager/service/UsuarioService.java
@@ -5,6 +5,7 @@ import br.com.posfiap.restmanager.error.NotFoundException;
 import br.com.posfiap.restmanager.mapper.UsuarioMapper;
 import br.com.posfiap.restmanager.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import static java.text.MessageFormat.format;
@@ -15,9 +16,11 @@ public class UsuarioService {
 
     private final UsuarioMapper usuarioMapper;
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Usuario incluir(Usuario usuario) {
 
+        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
         var usuarioEntity = usuarioRepository.save(usuarioMapper.mapToUsuarioEntity(usuario));
 
         return usuarioMapper.mapToUsuario(usuarioEntity);
